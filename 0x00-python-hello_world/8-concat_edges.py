4b9e7af9dee58525ce0e674ed7bd6eb7f4a938f0
saidachnid <achni.said@gmail.com>
2023-10-30 06:41:48 -0700

    ;e

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[107:112] + str[:6]
-print(str)
-
+str1 = "object-"
+str2 = "programming"
+str3 = " with "
+str4 = "Python"
+print(str1 + str2 + str3 + str4)
