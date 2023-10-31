3f66ba0bb07f660b5a0db89dd74e0f4540100539
saidachnid <achni.said@gmail.com>
2023-10-31 16:14:23 -0700

    alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for alphabet in range(ord('a'),ord('z') + 1);
-print("{}",format(chr(alphabet)), end ="")
+for i in range (ord('a'), ord('z') + 1):
+    print"(:c)".format (i), end="")
