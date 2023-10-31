2a8261e95bba6701d32d4d176182aa5ee2e28429
saidachnid <achni.said@gmail.com>
2023-10-31 08:37:53 -0700

    aaaa

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print(number, end=" ")
+if number > 5
+print ('is greater than 5')
+elif number == 0:
+    print ('is 0')
+elif number < 6
+print (' is less than 6 and not 0')
