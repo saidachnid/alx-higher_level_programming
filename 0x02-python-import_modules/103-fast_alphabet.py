0643f6d32476c73192bff335a126ed9b462ffcd3
saidachnid <achni.said@gmail.com>
2023-11-02 17:30:27 -0700

    aaas

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
-import string
-print(string.ascii_uppercase)
+def print_alphabet(letter):
+    print(letter, end='')
+    if letter < 'Z':
+        print_alphabet(chr(ord(letter) + 1))
+
+if __name__ == "__main__":
+    print_alphabet('A')
+    print()
 
