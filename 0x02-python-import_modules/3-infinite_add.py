6b7d5ad4fd24888ad55989b0851b1f3b3825f474
saidachnid <achni.said@gmail.com>
2023-11-02 19:28:40 -0700

    qdq

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
-#  Prints the number of and the list of its arguments
+
 if __name__ == "__main__":
+    """Print the addition of all arguments."""
     import sys
 
-    arg = sys.argv
-    size = len(arg) - 1
-
-    if size > 1:
-        print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
-
-    elif size == 0:
-        print("{} arguments.".format(size))
-
-    else:
-        print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))
-
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
+~                                                                                                                     
+~                                
