bd60609c352178fc189efa507d6308fd2cb4390a
saidachnid <achni.said@gmail.com>
2023-11-02 19:51:54 -0700

    faa

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def print_alphabet(letter):
-    print(letter, end='')
-    if letter < 'Z':
-        print_alphabet(chr(ord(letter) + 1))
-
-if __name__ == "__main__":
-    print_alphabet('A')
-    print()
-
+import string
+print(string.ascii_uppercase)
