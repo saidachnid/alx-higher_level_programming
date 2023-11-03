011fa7e5988f034dfb242ddabf1d9b3ceff92581
saidachnid <achni.said@gmail.com>
2023-11-02 19:34:34 -0700

    ers

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,3 @@
-#!/bin/python3
-[print("#pythoniscool")]
-
+#!/usr/bin/python3
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+~                                                             
