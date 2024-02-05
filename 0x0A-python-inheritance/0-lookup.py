1b38dbc8280cbc6478c0f92aef25d915400dd577
said achnid <achni.said@gmail.com>
2024-02-05 08:08:38 -0800

    ss

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''modile for lookup method'''
+def lookup (obj)
+''''looks up object ettributes and methods'''
+args:
+    obj (object) : the object to list.
+    returns: 
+        list: this list of ettributes.
+''''
+return dir(obj)
